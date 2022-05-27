d26a87d181194da633d42f0d9d542d33f592cc3b
axyz <axyz@Nileshs-MacBook-Air.local>
2022-05-27 18:55:05 +0530

    Lecture 18

---
+++ Lec18/WavePrint.java
@@ -0,0 +1,53 @@
+package Lec18;
+
+public class WavePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+		Hwave(arr);
+
+	}
+	public static void Hwave(int [][] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(i%2==0)
+			{
+				for(int j = 0; j < arr[0].length;j++)
+				{
+					System.out.print(arr[i][j]+" ");
+				}
+			}
+			else
+			{
+				for(int j = arr[0].length-1;j >= 0;j--)
+				{
+					System.out.print(arr[i][j] +" ");
+				}
+			}
+		}
+	}
+	
+	public static void Vwave(int [][] arr)
+	{
+		for(int j = 0; i < arr.length; i++)
+		{
+			if(i%2==0)
+			{
+				for(int j = 0; j < arr[0].length;j++)
+				{
+					System.out.print(arr[i][j]+" ");
+				}
+			}
+			else
+			{
+				for(int j = arr[0].length-1;j >= 0;j--)
+				{
+					System.out.print(arr[i][j] +" ");
+				}
+			}
+		}
+	}
+
+}
